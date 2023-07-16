d71ebdcc831ae2ae0d53f6a40005cabe19a7a15c
scriptax <moradi.online@outlook.com>
2023-07-17 00:23:15 +0330

    initial commit - intial project setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,tsx}"],
+  theme: {
+    extend: {
+    },
+    fontFamily: {
+      // primary: ["Overpass", "Tahoma", "sans serif"]
+    }
+  },
+  darkMode: 'class',
+  plugins: [],
+}
+
