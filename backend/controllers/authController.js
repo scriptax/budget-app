8fae1f61283ddcfa18bc46bd358118b7061a49fb
scriptax <moradi.online@outlook.com>
2023-07-20 15:32:38 +0330

    added logout endpoint

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -69,3 +69,11 @@ exports.login = catchAsync(async (req, res, next) => {
   // Everything ok... send token
   createAndSendToken(user, 200, req, res);
 });
+
+exports.logout = (req, res, next) => {
+  res.cookie("jwt", "loggedout", {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true
+  });
+  res.status(200).json({ status: "success" });
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,11 +3,11 @@ const express = require("express");
 const authController = require("./../controllers/authController");
 
 const router = express.Router();
-console.log(authController);
+
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
+router.get("/logout", authController.logout);
 // router.post("/test", authController.test);
-// router.post("/logout", authController.logout);
 // router.post("/account", authController.account);
 // router.post("/updatePassword", authController.updatePassword);
 // router.post("/deleteAccount", authController.deleteAccount);
