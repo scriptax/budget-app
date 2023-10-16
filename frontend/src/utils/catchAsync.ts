502e6d22210a5749e2f54fe73373440c4b53a196
scriptax <moradi.online@outlook.com>
2023-10-16 19:11:11 +0330

    fixed dashboard bug

--- frontend/src/utils/catchAsync.ts
+++ frontend/src/utils/catchAsync.ts
@@ -1,16 +1,19 @@
 import { AxiosError } from "axios";
 import { toast } from "react-toastify";
 
-export default function catchAsync (func: () => Promise<any>) {
-  return func().catch((error) => {
-    console.log("The error is: ", error)
+export default async function catchAsync (func: () => Promise<any>) {
+  try {
+    return await func();
+  } catch(error) {
     if (error instanceof AxiosError) {
       if (error.response) {
         // The server responded with a status code that is not 2xx
-        return toast.error(error.response.data.message);
+        toast.error(error.response.data.message);
+        return null;
       } else {
-        return toast.error("Something went very wrong!");
+        toast.error("Something went very wrong!");
+        return null;
       }
     }
-  });
+  }
 }
